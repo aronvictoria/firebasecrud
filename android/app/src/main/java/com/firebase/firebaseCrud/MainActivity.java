c00b25f2c6c20e148b158e7f2412a81a607d7a96
aronvictoria <aronmvictoria@gmail.com>
2023-07-18 01:52:59 +0800

    ionicAndroid

---
+++ android/app/src/main/java/com/firebase/firebaseCrud/MainActivity.java
@@ -0,0 +1,5 @@
+package com.firebase.firebaseCrud;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBl9W9-wybBeYIN7s8fZR4SSnLj4lU5arY",
+    authDomain: "ionicfirebase-1aa55.firebaseapp.com",
+    projectId: "ionicfirebase-1aa55",
+    storageBucket: "ionicfirebase-1aa55.appspot.com",
+    messagingSenderId: "99754220124",
+    appId: "1:99754220124:web:c0be969fb1026793e5a499",
+    measurementId: "G-YD9RKG3R8G"
+  }
 };
+
